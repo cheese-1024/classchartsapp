aeab21f8768b185518f936540f2c2c831f850890
Tommy Coleman <tommypickaxe@gmail.com>
2022-03-12 14:40:03 +0000

    broken

---
+++ App.js
@@ -0,0 +1,94 @@
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import DatePicker from 'react-native-date-picker';
+import {ClasschartsClient} from 'classcharts-api';
+
+function LoginPage() {
+  const [code, setCode] = useState('');
+  const [text, setText] = useState('');
+  const [date, setDate] = useState(new Date());
+  
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.center}>Enter your User Code:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={newCode => setCode(newCode)}
+          defaultValue={code}
+        />
+        <Text style={[styles.marginTop20, styles.redText]}>
+          Error Placeholder
+        </Text>
+      </View>
+      <View>
+        <Text style={[styles.center, styles.marginTop50]}>
+          Enter your date of birth:
+        </Text>
+        <DatePicker
+          date={date}
+          onDateChange={setDate}
+          fadeToColor={'none'}
+          mode={'date'}
+        />
+        <Button
+          onPress={login()}
+          title="Submit"
+          accessibilityLabel="Click here to submit your information"
+        />
+      </View>
+    </View>
+  );
+  async function login() {
+    const client = new ClasschartsClient({code}, {date});
+    await client.login();
+    setText(await client.getLessons());
+  }
+}
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Log in to ClassCharts" component={LoginPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+
+  textInput: {
+    borderWidth: 1,
+    width: 200,
+    marginTop: 5,
+  },
+
+  center: {
+    textAlign: 'center',
+  },
+
+  marginTop50: {
+    marginTop: 50,
+  },
+
+  marginTop20: {
+    marginTop: 20,
+  },
+
+  redText: {
+    color: 'red',
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# ClassCharts
+An improved React Native ClassCharts app for Android and iOS. Original: https://www.classcharts.com/
